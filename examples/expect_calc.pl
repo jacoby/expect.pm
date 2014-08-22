724145b9f5b32738df4e601e2933d9a440f83015
Gabor Szabo <gabor@szabgab.com>
2014-08-22 06:24:24 +0300

    calc expect

--- examples/expect_calc.pl
+++ examples/expect_calc.pl
@@ -9,9 +9,15 @@ $e->spawn($^X, "examples/calc.pl") or die;
 $e->log_stdout(0);
 $e->raw_pty(1);
 $e->send("19+23\n");
+$e->expect(1, "19+23");
+print 'Match: <', $e->match, ">\n";
+print 'Before: <', $e->before, ">\n";
+print 'After: <', $e->after, ">\n";
+$e->clear_accum;
+
 $e->expect(1, '-re' => qr/'\d+'/);
-print 'Match: ', $e->match, "\n";
-print 'Before: ', $e->before, "\n";
-print 'After: ', $e->after, "\n";
+print 'Match: <', $e->match, ">\n";
+print 'Before: <', $e->before, ">\n";
+print 'After: <', $e->after, ">\n";
 $e->close;
 
